4919930a9d98f99665383afe32d79974ee88aade
Tianyu <t.zhang@nki.nl>
2023-10-02 10:34:44 +0200

    Update config.py

--- scr/config.py
+++ scr/config.py
@@ -2,11 +2,12 @@
 Dataset configurations:
   :param TRAIN_FILEPATH -> the directory path to dataset files of training
   :param VALID_FILEPATH -> the directory path to dataset files of validation
+  :param SAVE_PATH -> the directory path to save plot and the generator model
 """"
 
 TRAIN_FILEPATH = "./TRAIN_files/"      # Example: Train_1.npz, Train_2.npz, Train_3.npz ... (src_images1, src_images2, src_images3, tar_images)
 VALID_FILEPATH = "./VAILD_files/Validation.npz"      # Valid.npz (src_images1, src_images2, src_images3, tar_images)
-
+SAVE_PATH = "./SAVE_IMPORTANT-NET/"
 
 """"
 Training configurations:
