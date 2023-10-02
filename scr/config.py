56b9a57cdb0f3221137eaed897ae7aecdd64ad54
Tianyu <t.zhang@nki.nl>
2023-10-02 10:31:20 +0200

    Update config.py

--- scr/config.py
+++ scr/config.py
@@ -5,7 +5,7 @@ Dataset configurations:
 """"
 
 TRAIN_FILEPATH = "./TRAIN_files/"      # Example: Train_1.npz, Train_2.npz, Train_3.npz ... (src_images1, src_images2, src_images3, tar_images)
-VALID_FILEPATH = "./VAILD_files/"      # Valid.npz (src_images1, src_images2, src_images3, tar_images)
+VALID_FILEPATH = "./VAILD_files/Validation.npz"      # Valid.npz (src_images1, src_images2, src_images3, tar_images)
 
 
 """"
